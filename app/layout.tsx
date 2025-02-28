ef54dd2ae1149930339a080a467eca0e63bf1869
Yumi <queen0701@gmail.com>
2025-03-01 00:17:15 +0900

    초기 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import 'css/tailwind.css'
 import 'pliny/search/algolia.css'
 import 'remark-github-blockquote-alert/alert.css'
 
-import { Space_Grotesk } from 'next/font/google'
+import { Noto_Sans_KR } from 'next/font/google'
 import { Analytics, AnalyticsConfig } from 'pliny/analytics'
 import { SearchProvider, SearchConfig } from 'pliny/search'
 import Header from '@/components/Header'
@@ -12,10 +12,11 @@ import siteMetadata from '@/data/siteMetadata'
 import { ThemeProviders } from './theme-providers'
 import { Metadata } from 'next'
 
-const space_grotesk = Space_Grotesk({
+const notoSansKr = Noto_Sans_KR({
+  // preload: true,
   subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-space-grotesk',
+  weight: ['100', '400', '700', '900'],
+  variable: '--font-noto-sans-kr',
 })
 
 export const metadata: Metadata = {
@@ -64,7 +65,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html
       lang={siteMetadata.language}
-      className={`${space_grotesk.variable} scroll-smooth`}
+      className={`${notoSansKr.variable} scroll-smooth`}
       suppressHydrationWarning
     >
       <link
--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -32,16 +32,8 @@ export default function AuthorLayout({ children, content }: Props) {
             )}
             <h3 className="pt-4 pb-2 text-2xl leading-8 font-bold tracking-tight">{name}</h3>
             <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
-            <div className="text-gray-500 dark:text-gray-400">{company}</div>
-            <div className="flex space-x-3 pt-6">
-              <SocialIcon kind="mail" href={`mailto:${email}`} />
-              <SocialIcon kind="github" href={github} />
-              <SocialIcon kind="linkedin" href={linkedin} />
-              <SocialIcon kind="x" href={twitter} />
-              <SocialIcon kind="bluesky" href={bluesky} />
-            </div>
           </div>
-          <div className="prose dark:prose-invert max-w-none pt-8 pb-8 xl:col-span-2">
+          <div className="prose dark:prose-invert max-w-none pt-8 pb-8 leading-snug xl:col-span-2">
             {children}
           </div>
         </div>
